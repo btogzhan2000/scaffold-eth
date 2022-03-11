b064a87f8ade9ccbbec069fbd83cabb9361af3ee
Togzhan Barakbayeva <45527668+btogzhan2000@users.noreply.github.com>
2022-03-11 18:16:28 +0600

    added code for simple nft example challenge

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,27 +1,121 @@
 pragma solidity >=0.8.0 <0.9.0;
-//SPDX-License-Identifier: MIT
 
-import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract YourContract {
+contract YourContract is ERC721,ERC721Enumerable, ERC721URIStorage, Ownable {
 
-  event SetPurpose(address sender, string purpose);
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
 
-  string public purpose = "Building Unstoppable Apps!!!";
+  uint256 public price;
+  uint256 constant startingAt = 0.01 ether;
 
-  constructor() payable {
-    // what should we do on deploy?
+  mapping (bytes32 => bool) public forSale;
+  mapping (bytes32 => bool) public forSecondarySale;
+  mapping (bytes32 => uint256) public uriToTokenId;
+
+  constructor(bytes32[] memory assetsForSale) public ERC721("YourContract", "YC") {
+    for(uint256 i=0;i<assetsForSale.length;i++){
+      forSale[assetsForSale[i]] = true;
+      forSecondarySale[assetsForSale[i]] = false;
+    }
+    price = startingAt;
   }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      emit SetPurpose(msg.sender, purpose);
+  function _baseURI() internal pure override returns (string memory) {
+    return "https://ipfs.io/ipfs/";
   }
 
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+  function mintItem(string memory tokenURI) public payable returns (uint256) {
+    bytes32 uriHash = keccak256(abi.encodePacked(tokenURI));
+
+    require(forSale[uriHash], "error");
+    forSale[uriHash]=false;
+
+    _tokenIds.increment();
+
+    uint256 id = _tokenIds.current();
+    _mint(msg.sender, id);
+    _setTokenURI(id, tokenURI);
+
+    uriToTokenId[uriHash] = id;
+    return id;
+  }
+
+  function sellItem(string memory tokenURI) public returns (uint256) {
+    bytes32 uriHash = keccak256(abi.encodePacked(tokenURI));
+
+    require(!forSecondarySale[uriHash], "error");
+    forSecondarySale[uriHash]=true;
+
+    uint256 id = uriToTokenId[uriHash];
+
+    return id;
+  }
+
+  function cancelSellItem(string memory tokenURI) public returns (uint256) {
+    bytes32 uriHash = keccak256(abi.encodePacked(tokenURI));
+
+    require(forSecondarySale[uriHash],"error");
+    forSecondarySale[uriHash]=false;
+
+    uint256 id = uriToTokenId[uriHash];
+
+    return id;
+  }
+
+  function buyItem(string memory tokenURI) public payable returns (uint256) {
+    bytes32 uriHash = keccak256(abi.encodePacked(tokenURI));
+
+    require(forSecondarySale[uriHash], "error");
+    forSecondarySale[uriHash]=false;
+
+    uint256 id = uriToTokenId[uriHash];
+    address payable itemOwner = payable(ownerOf(id));
+
+    _transfer(itemOwner, msg.sender, id);
+    itemOwner.transfer(price);
+
+    return id;
+  }
+
+// required default override functions (not my own code)
+  function _beforeTokenTransfer(
+      address from,
+      address to,
+      uint256 tokenId
+  ) internal override(ERC721, ERC721Enumerable) {
+      super._beforeTokenTransfer(from, to, tokenId);
+  }
+
+  function _burn(uint256 tokenId)
+      internal
+      override(ERC721, ERC721URIStorage)
+  {
+      super._burn(tokenId);
+  }
+
+  function tokenURI(uint256 tokenId)
+      public
+      view
+      override(ERC721, ERC721URIStorage)
+      returns (string memory)
+  {
+      return super.tokenURI(tokenId);
+  }
+
+  function supportsInterface(bytes4 interfaceId)
+      public
+      view
+      override(ERC721, ERC721Enumerable)
+      returns (bool)
+  {
+      return super.supportsInterface(interfaceId);
+  }
 }
+
+
